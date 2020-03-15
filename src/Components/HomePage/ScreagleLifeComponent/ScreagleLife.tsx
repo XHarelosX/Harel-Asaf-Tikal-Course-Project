afddb7e457a4ff6ca8e1f07c4935f76c0b7ca639
Harel Asaf <harelasaf1@gmail.com>
2020-03-15 20:54:33 +0200

    Update Homepage & ScteagleLife Css

--- src/Components/HomePage/ScreagleLifeComponent/ScreagleLife.tsx
+++ src/Components/HomePage/ScreagleLifeComponent/ScreagleLife.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import classes from "./ScreagleLife.module.css"
 import img1 from "../../../Images/cheerleaders.jpg"
 import img2 from "../../../Images/party.jpg"
@@ -13,7 +13,7 @@ interface Props {
 
 const ScreagleLife: React.FC<Props> = () => {
     return (
-        <Fragment>
+        <>
             <div className={classes.title_conteiner}>
                 <hr className={classes.hr}></hr>
                 <h2 className={classes.h2}>Explore ScreagleLife</h2>
@@ -23,35 +23,65 @@ const ScreagleLife: React.FC<Props> = () => {
                 <li className={classes.ul_li}>
                     <div className={classes.ul_li_div}>
                         <img className={classes.div_img} src={img1} alt="" />
+                        <div className={classes.div_title}>
+                            <h2 className={classes.h2_title}>School Spirit</h2>
+                            <hr className={classes.title_hr}></hr>
+                            <p className={classes.title_p}>Sed porttitor lectus nibh. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Donec rutrum congue leo eget malesuada. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Proin eget tortor risus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.</p>
+                        </div>
                     </div>
                 </li>
                 <li className={classes.ul_li}>
                     <div className={classes.ul_li_div}>
                         <img className={classes.div_img} src={img2} alt="" />
+                        <div className={classes.div_title}>
+                            <h2 className={classes.h2_title}>Housing</h2>
+                            <hr className={classes.title_hr}></hr>
+                            <p className={classes.title_p}>Sed porttitor lectus nibh. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Donec rutrum congue leo eget malesuada. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Proin eget tortor risus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.</p>
+                        </div>
                     </div>
                 </li>
                 <li className={classes.ul_li}>
                     <div className={classes.ul_li_div}>
                         <img className={classes.div_img} src={img3} alt="" />
+                        <div className={classes.div_title}>
+                            <h2 className={classes.h2_title}>Student Involvement</h2>
+                            <hr className={classes.title_hr}></hr>
+                            <p className={classes.title_p}>Sed porttitor lectus nibh. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Donec rutrum congue leo eget malesuada. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Proin eget tortor risus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.</p>
+                        </div>
                     </div>
                 </li>
                 <li className={classes.ul_li}>
                     <div className={classes.ul_li_div}>
                         <img className={classes.div_img} src={img4} alt="" />
+                        <div className={classes.div_title}>
+                            <h2 className={classes.h2_title}>Club Sports</h2>
+                            <hr className={classes.title_hr}></hr>
+                            <p className={classes.title_p}>Sed porttitor lectus nibh. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Donec rutrum congue leo eget malesuada. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Proin eget tortor risus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.</p>
+                        </div>
                     </div>
                 </li>
                 <li className={classes.ul_li}>
                     <div className={classes.ul_li_div}>
                         <img className={classes.div_img} src={img5} alt="" />
+                        <div className={classes.div_title}>
+                            <h2 className={classes.h2_title}>Recreation & Intramurals</h2>
+                            <hr className={classes.title_hr}></hr>
+                            <p className={classes.title_p}>Sed porttitor lectus nibh. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Donec rutrum congue leo eget malesuada. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Proin eget tortor risus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.</p>
+                        </div>
                     </div>
                 </li>
                 <li className={classes.ul_li}>
                     <div className={classes.ul_li_div}>
                         <img className={classes.div_img} src={img6} alt="" />
+                        <div className={classes.div_title}>
+                            <h2 className={classes.h2_title}>Student Community</h2>
+                            <hr className={classes.title_hr}></hr>
+                            <p className={classes.title_p}>Sed porttitor lectus nibh. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Donec rutrum congue leo eget malesuada. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Proin eget tortor risus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui.</p>
+                        </div>
                     </div>
                 </li>
             </ul>
-        </Fragment >
+        </>
     )
 }
 
