56e7deb31962c35840df9d97489079f764223824
Harel Asaf <harelasaf1@gmail.com>
2020-03-22 00:13:54 +0200

    add all app to Client folder

---
+++ build/precache-manifest.0cc4b46b7b94a02d1052faf53668e732.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c3c5f8e210a617c74074afca06ab0d2",
+    "url": "/Harel-Asaf-Tikal-Course-Project/index.html"
+  },
+  {
+    "revision": "485b3e60dd06dea48874",
+    "url": "/Harel-Asaf-Tikal-Course-Project/static/css/main.d35de2a7.chunk.css"
+  },
+  {
+    "revision": "7dbfb6f9d167d53630b4",
+    "url": "/Harel-Asaf-Tikal-Course-Project/static/js/2.c5dac83a.chunk.js"
+  },
+  {
+    "revision": "0bdc4f0d8b3d84ae6a73bb9902b479ee",
+    "url": "/Harel-Asaf-Tikal-Course-Project/static/js/2.c5dac83a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "485b3e60dd06dea48874",
+    "url": "/Harel-Asaf-Tikal-Course-Project/static/js/main.71890ce3.chunk.js"
+  },
+  {
+    "revision": "7a09c8e9b69ff539d104",
+    "url": "/Harel-Asaf-Tikal-Course-Project/static/js/runtime-main.40e6f782.js"
+  },
+  {
+    "revision": "38909fc6f9e5d3b5f5f3341675514676",
+    "url": "/Harel-Asaf-Tikal-Course-Project/static/media/Monsters-University-Clips.38909fc6.jpg"
+  },
+  {
+    "revision": "e93fdc2ab5aa56c2aa5cf63ef983d2e5",
+    "url": "/Harel-Asaf-Tikal-Course-Project/static/media/Pixar_MU.e93fdc2a.jpg"
+  },
+  {
+    "revision": "3bbd9d4b0d737701dae3cad1ac589bf5",
+    "url": "/Harel-Asaf-Tikal-Course-Project/static/media/cheerleaders.3bbd9d4b.jpg"
+  },
+  {
+    "revision": "2386dfce3581d8dd92a471aaeec5acd2",
+    "url": "/Harel-Asaf-Tikal-Course-Project/static/media/maxresdefault.2386dfce.jpg"
+  },
+  {
+    "revision": "5112f358eb21ecb0a9d50538928a365d",
+    "url": "/Harel-Asaf-Tikal-Course-Project/static/media/monsters-university.5112f358.jpg"
+  },
+  {
+    "revision": "0896117cf0c4456555282058e2ea543f",
+    "url": "/Harel-Asaf-Tikal-Course-Project/static/media/monstersuseaurchin.0896117c.jpg"
+  },
+  {
+    "revision": "3d61ac4aee548496559d31af42667343",
+    "url": "/Harel-Asaf-Tikal-Course-Project/static/media/party.3d61ac4a.jpg"
+  }
+]);
